d935cb05464964b494096206fdc475895e310f4b
Hou Mengly <hourmengly74@gmail.com>
2025-03-15 17:37:58 +0700

    tp01

---
+++ lib/profile_section.dart
@@ -0,0 +1,53 @@
+// profile_section.dart
+import 'package:flutter/material.dart';
+
+class ProfileSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 120,
+              backgroundImage: AssetImage("assets/images/photo.jpg"),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Welcome to my portfolio",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            Text(
+              "Hi, I'm",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+            ),
+            Text(
+              "HOU Mengly",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w700,
+                color: Colors.blue,
+              ),
+            ),
+            Text(
+              "Web Developer",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+            ),
+            Text(
+              "Web developer skilled in building modern,",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+            ),
+            Text(
+              "responsive, and efficient web applications. ",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+            ),
+            //
+          ],
+        ),
+      ],
+    );
+  }
+}
